58e866a710f6693a897995ec513ca420c4c9ff52
Filip Allberg <elitebeatstreet@gmail.com>
2019-06-07 15:36:13 +0200

    Clarifying comments for posterity

--- rda_ai/vitamins.py
+++ rda_ai/vitamins.py
@@ -35,12 +35,14 @@ all_vitamins = join_dictionaries(b_vitamins, other_vitamins)
 
 # Map 'Vitamin_A' to 'Vitamin A' as well (no underscore). Remove duplicates using set
 all_vitamins = {k: list(set([k.replace('_', ' ')] + v)) for k, v in all_vitamins.items()}
-assert(set(all_vitamins['Vitamin_A']) ==  set(['Vitamin A', 'Vitamin_A']))
+assert(set(all_vitamins['Vitamin_A']) ==  set(['Vitamin A', 'Vitamin_A'])) # Using set to not care about order
+assert(all_vitamins['Choline'] == ['Choline']) # No duplicates
 
 Vitamins = Enum('Vitamins', 
     names=itertools.chain.from_iterable(
         itertools.product(v, [k]) for k, v in all_vitamins.items()
         ))
 
+# We now have support for aliases and alternate spellings
 assert(Vitamins.Vitamin_B1 == Vitamins['Vitamin B1'] == Vitamins['Vitamin_B1'] == Vitamins['Thiamine'] == Vitamins['Thiamin'])
 assert(Vitamins.Vitamin_A == Vitamins['Vitamin_A'] == Vitamins['Vitamin A'])
